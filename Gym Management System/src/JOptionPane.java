153767194465dace2eb50010fa03430ce4771689
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2024-10-21 01:32:44 +0600

    Add files via upload

---
+++ Gym Management System/src/JOptionPane.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author UseR
+ */
+class JOptionPane {
+
+    static void showMessageDialog(Object object, Exception e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showConfirmDialog(Object object, String payment_is_already_done) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void showMessageDialog(Object object, String successfully_Updated) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
